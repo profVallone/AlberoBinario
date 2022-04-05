0015e3b64e9a3c48bc98aa4daf45f4695c58bcbb
profVallone <profvallone@gmail.com>
2022-04-05 09:59:57 +0200

    Aggiungere i file di progetto.

---
+++ AlberoBinarioIntero.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+
+
+namespace ProgettoAlbero
+{
+    internal class AlberoBinarioIntero
+    {
+        private int val;
+        private AlberoBinarioIntero sx;
+        private AlberoBinarioIntero dx;
+       
+        public AlberoBinarioIntero(int val, AlberoBinarioIntero sx, AlberoBinarioIntero dx)
+        {
+            this.val = val;
+            this.sx = sx;
+            this.dx = dx;
+        }
+
+        public AlberoBinarioIntero(int v)
+        {
+            this.val = v;
+            this.sx = null;
+            this.dx = null;
+        }
+
+        public void aggiungiFiglioSx(AlberoBinarioIntero a)
+        {
+            this.sx = a;
+        }
+
+        public void aggiungiFiglioDx(AlberoBinarioIntero a)
+        {
+            this.dx = a;
+        }
+
+        public void stampa()
+        {
+            if (this.sx == null && this.dx == null)
+            {
+                System.Console.WriteLine(this.val);
+            }
+            else
+            {
+                System.Console.WriteLine(this.val);
+                try
+                {
+                    this.sx.stampa();
+                }
+                catch (Exception e) {}
+                try
+                {
+                    this.dx.stampa();
+                }
+                catch (Exception e) { }
+            }
+        }
+
+        public void stampaIterativa()
+        {
+            Stack s = new Stack();
+            s.Push(this);
+            AlberoBinarioIntero tmp = this;
+            while (s.Count != 0)
+            {
+                //mi muovo sul ramo di sinistra
+                do
+                {
+                    System.Console.WriteLine(tmp.val);
+                    s.Push(tmp);
+                    tmp = tmp.sx;
+                } while (tmp != null);
+                
+                //risalgo l'albero finchè non trovo un sottoalbero di destra
+                do 
+                {
+                    tmp = s.Pop() as AlberoBinarioIntero;
+                } while (tmp.dx == null);
+                //aggiorno il nodo temporaneo
+                tmp = tmp.dx;
+            }  
+        }
+
+        public void stampaIterativa2()
+        {
+            Stack s = new Stack();
+            s.Push(this);
+            while (s.Count != 0)
+            {
+                //mi muovo sul ramo di sinistra
+                AlberoBinarioIntero tmp = s.Pop() as AlberoBinarioIntero;
+                Console.WriteLine(tmp.val);
+                if (tmp.dx != null)
+                    s.Push(tmp.dx);
+                if (tmp.sx != null)
+                    s.Push(tmp.sx);
+            }
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+            if (this.sx == null && this.dx == null)
+            {
+                return "(" + this.val + ")";
+            }
+            else
+            {
+                s += "(" + this.val;
+                if(this.sx != null)
+                    s += this.sx.ToString();
+                if (this.dx != null)
+                    s += this.dx.ToString() + ")";
+            }
+            return s;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+
+namespace ProgettoAlbero
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AlberoBinarioIntero sx;
+            AlberoBinarioIntero dx;
+            AlberoBinarioIntero p;
+            AlberoBinarioIntero r = new AlberoBinarioIntero(69);
+          
+            sx = new AlberoBinarioIntero(2);
+            dx = new AlberoBinarioIntero(71);
+            p = new AlberoBinarioIntero(39, sx, dx);
+            sx = p;
+            dx = new AlberoBinarioIntero(66);
+            dx.aggiungiFiglioDx(new AlberoBinarioIntero(77));
+            
+            r.aggiungiFiglioSx(new AlberoBinarioIntero(89, sx, dx));
+            
+            sx = new AlberoBinarioIntero(44);
+            dx = new AlberoBinarioIntero(12);
+            
+            r.aggiungiFiglioDx(new AlberoBinarioIntero(28, sx, dx));
+
+            r.stampa();
+            Console.WriteLine("----------------");
+            Console.WriteLine(r);
+            Console.WriteLine("--Iterativa--------------");
+            r.stampaIterativa();
+            Console.WriteLine("--Iterativa2--------------");
+            r.stampaIterativa2();
+
+
+
+
+
+
+
+
+        }
+    }
+}
